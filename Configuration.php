aa237432f7988b2d3e62758a13c323b363555a48
Kristians-R <k.raikstins@gmail.com>
2020-07-23 16:48:01 +0300

    Connection with Database

---
+++ Configuration.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app;
+
+class Configuration
+{
+    public $dbHost = "localhost";
+    public $dbUserName = "root";
+    public $dbPassword = "";
+    public $dbName = "galadarbsbackend";
+    public $timeZone = "Europe/Riga";
+}
